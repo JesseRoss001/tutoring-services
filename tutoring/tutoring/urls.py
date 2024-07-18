f93db38fa468ffa8aeeef727165ce6f15ce220cc
Jesse Lee Ross <79084912+JesseRoss001@users.noreply.github.com>
2024-07-18 11:04:51 +0100

    update

--- tutoring/tutoring/urls.py
+++ tutoring/tutoring/urls.py
@@ -7,8 +7,5 @@ from core import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
-    path('signup/', views.signup, name='signup'),
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
   + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
